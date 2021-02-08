0908942daf1e21ff3fed328ae079e44f125cf77a
Mustafa KÜÇÜK <devmustafakucuk@gmail.com>
2021-02-08 12:33:19 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        questions: [{
+            q: 'Müslüman mısın?',
+            no: false,
+            yes: {
+                q: 'Türk müsün?',
+                no: false,
+                yes: {
+                    q: 'Kadın haklarını savunuyor musun?',
+                    yes: false,
+                    no: {
+                        q: 'Asgari ücretle dört kişilik bir aileyi geçindirebilir misin?',
+                        no: false,
+                        yes: {
+                            q: 'LGBTI+ haklarını savunuyor musun?',
+                            yes: {
+                                q: 'Lezbiyen Mezbiyen misin?',
+                                yes: 'Sen aslında yoksun!',
+                                no: false,
+                            },
+                            no: {
+                                q: 'Herhangi bir insan hakkını savunuyor musun?',
+                                yes: false,
+                                no: 'Hala terörist olma ihtiamliniz var, henüz tam liste açıklanmadı.'
+                            }
+                        }
+                    },
+                }
+            }
+        }],
+        currentQuestionIndex: 0,
+        currentQuestionData: {}
+    },
+    computed: {
+        currentQuestion: function () {
+            return this.currentQuestionData;
+        }
+    },
+    methods: {
+        submitAnswer: function (answer) {
+            const getAnswerData = this.currentQuestion[answer];
+            let errorMessage;
+
+            if (getAnswerData === false) {
+                errorMessage = 'Teröristsin!!!';
+            } else if (!(getAnswerData instanceof Object)) {
+                errorMessage = getAnswerData;
+            } else {
+                this.currentQuestionData = getAnswerData;
+            }
+
+            if (errorMessage) {
+                Swal.fire({
+                    title: errorMessage,
+                    icon: 'error',
+                })
+            }
+
+            return true;
+        }
+    },
+    mounted: function () {
+        this.currentQuestionData = this.questions[0];
+    }
+})
